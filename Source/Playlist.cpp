5e694c5f8c9a1ebc6b3cf1bfcf3f55527330a10c
Nick Fetcko <nick.fetcko@gmail.com>
2025-11-10 17:34:21 -0600

    Improving click detection in Playlist

--- Source/Playlist.cpp
+++ Source/Playlist.cpp
@@ -416,8 +416,10 @@ std::optional<Playlist::Track> Playlist::OnMouseClicked(const Vector2i &mousePos
 				std::distance(cue->GetCurrentTrack(), cue->GetTracks().end()) :
 				std::distance(currentFile, files.end());
 
+		auto &bounds = titles.begin()->GetBounds();
+
 		// If our offset is not 0, we need to account for the previous track
-		if (auto index = (mousePos.y - pos.y) / titles.begin()->GetBounds().height - (offset != 0 ? 1 : 0);
+		if (auto index = (mousePos.y - pos.y + bounds.y / 2) / bounds.height - (offset != 0 ? 1 : 0);
 			index < distance) {
 			if (mousePos.x >= pos.x && mousePos.x <= pos.x + titles[offset + index].GetSize().x) {
 				if (!files.empty()) {
